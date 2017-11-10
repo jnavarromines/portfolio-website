49a9f8d3ec327edca96e1fafe79b0eb80c02287f
Josiah Navarro <jnavarro@mymail.mines.edu>
2017-11-09 21:57:09 -0700

    Added hover css for nav items. Added footer, which gets animated after the nav bar.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   $("header").slideDown(750, function() {
-    $("#main-nav-bar").fadeIn("slow");
+    $("#main-nav-bar").fadeIn("slow", function() {
+      $("footer").fadeIn("slow");
+    });
   });
 });
